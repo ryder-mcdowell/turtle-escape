e3aeca8b434c8175fa313d2e31dc214fff3a4b62
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-04-08 20:35:21 -0700

    Initial commit. Draw turtle and move several steps forward

---
+++ tony-the-turtle.py
@@ -0,0 +1,15 @@
+import turtle
+
+if __name__ == '__main__':
+    wn = turtle.Screen()
+    tony = turtle.Turtle()
+
+    tony.forward(25)
+    tony.forward(25)
+    tony.forward(25)
+    tony.forward(25)
+    tony.forward(25)
+    tony.forward(25)
+    tony.forward(25)
+
+    turtle.mainloop()
