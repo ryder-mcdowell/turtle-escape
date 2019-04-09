5ca2f28f48dceb27156cdc950345456b6af31874
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-04-08 21:32:23 -0700

    Add square drawing functions

--- escape.py
+++ escape.py
@@ -1,4 +1,5 @@
 import turtle
+import pickle
 
 def draw_bag():
     turtle.shape('turtle')
@@ -18,6 +19,10 @@ def escaped(position):
     y = int(position[1])
     return x < -35 or x > 35 or y < -35 or y > 35
 
+def store_position_data(L, t):
+    position = t.position()
+    L.append([position[0], position[1], escaped(position)])
+
 def draw_line():
     angle = 0
     step = 5
@@ -26,8 +31,32 @@ def draw_line():
         t.left(angle)
         t.forward(step)
 
+def draw_square(t, size):
+    L = []
+    for i in range(4):
+        t.forward(size)
+        t.left(90)
+        store_position_data(L, t)
+    return L
+
+def draw_squares(number):
+    t = turtle.Turtle()
+    L = []
+    for i in range(1, number + 1):
+        t.penup()
+        t.goto(-i,-i)
+        t.pendown()
+        L.extend(draw_square(t, i * 2))
+    return L
+
+def draw_squares_until_escaped(n):
+    t = turtle.Turtle()
+    L = draw_squares(n)
+    with open("data_square", "wb") as f:
+        pickle.dump(L, f)
+
 if __name__ == '__main__':
     turtle.setworldcoordinates(-70,-70,70,70)
     draw_bag()
-    draw_line()
+    draw_squares_until_escaped(10)
     turtle.mainloop()
