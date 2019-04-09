747b74d706f8e16b47f0a73791e81dd9f553e0dd
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-04-08 21:36:32 -0700

    Add arguments/arguments parsing

--- escape.py
+++ escape.py
@@ -1,6 +1,8 @@
 import turtle
 import pickle
 import random
+import argparse
+import inspect
 
 def draw_bag():
     turtle.shape('turtle')
@@ -88,8 +90,36 @@ def draw_random_spirangles():
     with open("data_rand", "wb") as f:
         pickle.dump(L, f)
 
+
 if __name__ == '__main__':
-    turtle.setworldcoordinates(-70,-70,70,70)
-    draw_bag()
-    draw_triangles(10)
-    turtle.mainloop()
+    fns = {
+        "line": draw_line,
+        "squares": draw_squares_until_escaped,
+        "triangles": draw_triangles,
+        "spirangles": draw_random_spirangles
+    }
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--function",
+        choices = fns,
+        help="One of " + ', '.join(fns.keys())
+    )
+    parser.add_argument("-n", "--number",
+        default = 50,
+        type=int,
+        help="How many?"
+    )
+    args = parser.parse_args()
+
+    try:
+        f = fns[args.function]
+        turtle.setworldcoordinates(-70,-70,70,70)
+        draw_bag()
+        turtle.hideturtle()
+        if len(inspect.getargspec(f).args) == 1:
+            f(args.number)
+        else:
+            f()
+        turtle.mainloop()
+    except KeyError:
+        parser.print_help()
