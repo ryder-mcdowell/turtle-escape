a6c1547ebb6bd83b00620f1eb998e4ce2e2901ae
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-04-08 21:29:35 -0700

    Add draw_line() function that draws until turtle has escaped

--- escape.py
+++ escape.py
@@ -13,7 +13,21 @@ def draw_bag():
     turtle.left(90)
     turtle.forward(70)
 
+def escaped(position):
+    x = int(position[0])
+    y = int(position[1])
+    return x < -35 or x > 35 or y < -35 or y > 35
+
+def draw_line():
+    angle = 0
+    step = 5
+    t = turtle.Turtle()
+    while not escaped(t.position()):
+        t.left(angle)
+        t.forward(step)
+
 if __name__ == '__main__':
     turtle.setworldcoordinates(-70,-70,70,70)
     draw_bag()
+    draw_line()
     turtle.mainloop()
